258395e2df655d909bb2daa04fc3f6cfb169699f
Steven Diamante <stevendiamante13@gmail.com>
2025-07-22 22:00:58 -0400

    Initial Commit

---
+++ src/test/java/com/diamantetechcoaching/GameTest.java
@@ -0,0 +1,13 @@
+package com.diamantetechcoaching;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+
+    @Test
+    void name() {
+        assertThat(true).isFalse();
+    }
+}
